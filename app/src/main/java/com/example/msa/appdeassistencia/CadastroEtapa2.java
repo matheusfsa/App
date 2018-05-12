d82a3f103ceceb9892c06d758f171cc798024da4
matheusfsa <tetofsa15@gmail.com>
2018-05-12 02:38:08 -0300

    integração com bd

--- app/src/main/java/com/example/msa/appdeassistencia/CadastroEtapa2.java
+++ app/src/main/java/com/example/msa/appdeassistencia/CadastroEtapa2.java
@@ -1,6 +1,7 @@
 package com.example.msa.appdeassistencia;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -10,9 +11,19 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+
 import classes.Pessoa;
+import util.IncluirAsyncTask;
 
 public class CadastroEtapa2 extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +43,8 @@ public class CadastroEtapa2 extends AppCompatActivity {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
                 Pessoa p = new Pessoa(nome.getText().toString(),sobrenome.getText().toString(),cpf.getText().toString(),telefone.getText().toString(), data_nascimento.getText().toString());
+                IncluirAsyncTask is = new IncluirAsyncTask(p, CadastroEtapa2.this) ;
+                is.execute();
                 Intent info2 = new Intent(CadastroEtapa2.this, CadastroEtapa3.class);
                 startActivity(info2);
             }
